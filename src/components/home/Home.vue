fbd447789e9f82e5046b902054c123fed6ac696e
Abell <abell@AbelldeMacBook-Pro.local>
2020-01-28 20:00:17 +0800

    project init

---
+++ src/components/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+<div class='home'>hello its your home</div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+.home{
+    font-size: 50px;
+}
+</style>
