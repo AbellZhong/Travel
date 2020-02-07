505ca9dbb01b618ca9b0250ec5c7b61f3507a928
Abell <abell@AbelldeMacBook-Pro.local>
2020-02-07 17:05:32 +0800

    performance

--- src/components/city/City.vue
+++ src/components/city/City.vue
@@ -2,8 +2,8 @@
   <div>
   <CityHeader></CityHeader>
   <CitySearch></CitySearch>
-  <CityList :cities='cities' :hotCities='hotCities'></CityList>
-  <CityAlphabet :cities='cities'></CityAlphabet>
+  <CityList :cities='cities' :hotCities='hotCities' :letter='letter'></CityList>
+  <CityAlphabet :cities='cities' @change='handleLetterChange'></CityAlphabet>
   </div>
 </template>
 
@@ -27,7 +27,8 @@ export default {
   data () {
     return {
       cities: {},
-      hotCities: []
+      hotCities: [],
+      letter: ''
     }
   },
   methods: {
@@ -40,6 +41,9 @@ export default {
         this.cities = res.data.cities
         this.hotCities = res.data.hotCities
       }
+    },
+    handleLetterChange (letter) {
+      this.letter = letter
     }
   }
 }
