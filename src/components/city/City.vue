471dd635c20cb7dd4d5e07c88a2d0a325a1acd4c
Abell <abell@AbelldeMacBook-Pro.local>
2020-02-07 14:24:51 +0800

    add search to city

--- src/components/city/City.vue
+++ src/components/city/City.vue
@@ -1,15 +1,27 @@
 <template>
+  <div>
   <CityHeader></CityHeader>
+  <CitySearch></CitySearch>
+  </div>
 </template>
 
 <script>
 import CityHeader from './component/header'
+import CitySearch from './component/search'
 export default {
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   }
 }
 </script>
 
-<style scoped lang='stylus'></style>
+<style scoped lang='stylus'>
+@import '~styles/varibles.styl'
+.search{
+    height .72rem
+    background $bgColor
+    padding 0 .1rem
+}
+</style>
